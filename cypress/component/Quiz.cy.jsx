8ee8a3508bedd4add4b8abf86db9eca5d738ba39
jaornelas <ornelalf001@gmail.com>
2025-04-19 20:20:32 -0500

    add other necessary starter files

---
+++ cypress/component/Quiz.cy.jsx
@@ -0,0 +1,22 @@
+import Quiz from "../.,/client/src/components/Quiz"
+
+describe ('Quiz Component', () => {
+    beforeEach(() => {
+        cy.intercept({
+            method: 'GET', 
+            url: '/api/questions/random'
+        },
+        {
+            fixture: 'questions.json',
+            statusCode:200
+        }
+    ).as('getRandomQuestion')
+    });
+
+it('should start the quiz and display the 1st question', () => {
+    cy.mount(<Quiz/>);
+    cy.get('button').contains('Start Quiz').click();
+    cy.get('.card').should('be.visible');
+    cy.get('h2').should('not.be.empty)')
+})
+})
